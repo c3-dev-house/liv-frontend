0001d24367516175cc146f518adc0c36fc20147e
louisdevc3 <117977837+louisdevc3@users.noreply.github.com>
2024-07-17 08:28:29 +0200

    louisdev | update axiosConfig to use aws backend

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/', //backend link for local: ...//localhost:3000/ uat: //'http://liv-village-backend-env.eba-2jjmrubm.eu-west-3.elasticbeanstalk.com/'
+  baseURL: 'http://liv-village-backend-env.eba-2jjmrubm.eu-west-3.elasticbeanstalk.com/', //backend link for local: ...//localhost:3000/ uat: //'http://liv-village-backend-env.eba-2jjmrubm.eu-west-3.elasticbeanstalk.com/'
 });
 
 export default axiosInstance;
