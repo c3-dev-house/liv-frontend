ac18efb4874cddd802e8132448bb86421c5db6b1
hermanmansC3 <herman@convergenc3.com>
2024-07-22 10:09:46 +0200

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/', //backend link for local: ...//localhost:3000/ uat: //'http://liv-village-backend-env.eba-2jjmrubm.eu-west-3.elasticbeanstalk.com/'
-  //http://liv.c3-dev-house.com/
-  //http://liv.c3-dev-house.com/
+  baseURL: 'https://liv.c3-dev-house.com/', //backend link for local: ...//localhost:3000/ uat: //'http://liv-village-backend-env.eba-2jjmrubm.eu-west-3.elasticbeanstalk.com/'
+  //https://liv.c3-dev-house.com/
+  //https://liv.c3-dev-house.com/
 });
 
 export default axiosInstance;
