5e9d59a7c72083e4230f7f37d05fd223597f4187
louisdevc3 <117977837+louisdevc3@users.noreply.github.com>
2024-07-24 15:18:03 +0200

    louisdev | redirect on 401 & 500 and added email to forgot pw screen

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { dummyUsers } from '../dummyUsers';
+//import { dummyUsers } from '../dummyUsers';
 import axios from "../axiosConfig";
 import handleAdminSignIn from "../../src/pages/SignIn";
 import { useNavigate } from 'react-router-dom';
