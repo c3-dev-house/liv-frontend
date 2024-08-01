d2564dabfa094667bacbce562c31178b212e720f
hermanmansC3 <herman@convergenc3.com>
2024-08-01 13:08:07 +0200

    Update Onboarding.jsx

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -220,7 +220,8 @@ const Onboarding = () => {
               value={formData.province}
               onChange={handleChange}
               className={`inputStyle ${formData.province ? 'blackText' : ''}`}
-            >
+            > 
+              <option value="">Select</option>
               {provinces.map((province, index) => (
                 <option key={index} value={province}>
                   {province}
